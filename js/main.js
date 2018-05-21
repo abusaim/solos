824bd8819518110007583dea4127c415756e5103
abusaim <abusaim1994@gmail.com>
2018-05-21 21:58:28 +0600

    Home Page done
    21-05-2018

--- js/main.js
+++ js/main.js
@@ -1,4 +1,36 @@
-var $ = jQuery;
+var $ = jQuery;	
+$(document).ready(function(){
+	// Scrol to top 
+  	$(window).scroll(function () {
+		if ($(this).scrollTop() > 767) {
+			$('.scrollup').fadeIn('slow');
+		} else {
+			$('.scrollup').fadeOut('slow');
+		}
+	});
+
+	$('.scrollup').click(function () {
+		$("html, body").animate({
+			scrollTop: 0
+		}, 1000);
+		return false;
+	});
+
+	//main Banner
+  	$('#main_banner').slick({
+    	dots: false,
+	  	infinite: true,
+	  	speed: 800,
+	 	fade: true,
+	 	autoplay: true,
+	 	autoplaySpeed:5000,
+	 	arrows: false,
+	 	touchMove: true,
+  	});
+});
+
+
+
 
 
 
