a4b2711ea357678bb92325a68058fe6bc9b5aa02
abusaim <abusaim1994@gmail.com>
2018-05-24 02:30:01 +0600

    Complete
    24-05-2018

--- js/main.js
+++ js/main.js
@@ -27,6 +27,43 @@ $(document).ready(function(){
 	 	arrows: false,
 	 	touchMove: true,
   	});
+  	// Main menu sticky	 
+	// var menu_height = $('#mainHeader').innerHeight();
+	// $(window).bind('scroll', function () {	 
+	//  if ($(window).scrollTop() >= num) {
+	//   		$('header+section').css({'margin-top':menu_height});
+	// 		$('#main_menu').addClass('sticky');
+	// 	} else {
+	// 		$('#main_menu').removeClass('sticky');
+	//   		$('header+section').css({'margin-top':0});
+	// 	}		
+	// });
+	/*
+	var nav = $('#main_menu');
+    var header = $('.headerTop');
+    var navHomeY = nav.offset().top;
+    var isFixed = false;
+    var $w = $(window);
+    $w.scroll(function () {
+        var scrollTop = $w.scrollTop();
+        var shouldBeFixed = scrollTop > navHomeY;
+        if (shouldBeFixed && !isFixed) {
+        	$('#main_menu').addClass('sticky');
+            nav.css({
+                position: 'fixed',
+                top: 0,                
+            });
+            header.css('marginTop', 50);
+            isFixed = true;
+        } else if (!shouldBeFixed && isFixed) {
+        	$('#main_menu').removeClass('sticky');
+            nav.css({
+                position: 'relative'
+            });
+            header.css('marginTop', 0);
+            isFixed = false;
+        }
+    });*/
 });
 
 
